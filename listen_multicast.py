bb4f954adb11db21242f3996415b8651b2c83e62
adowdy <nerael@gmail.com>
2017-10-10 19:49:32 -0700

    initial throw into git

---
+++ listen_multicast.py
@@ -0,0 +1,42 @@
+# Multicast client
+# Adapted from: http://chaos.weblogs.us/archives/164
+
+import socket
+
+ANY = "0.0.0.0" 
+# all hosts group
+MCAST_ADDR = '224.0.0.1' 
+#MCAST_ADDR = "239.255.255.250"
+MCAST_PORT = 3702
+
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+# Allow multiple sockets to use the same PORT number
+sock.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+#sock.setsockopt(socket.)
+
+# Bind to the port that we know will receive multicast data
+sock.bind((ANY,MCAST_PORT))
+
+# Tell the kernel that we want to add ourselves to a multicast group
+# The address for the multicast group is the third param
+status = sock.setsockopt(socket.IPPROTO_IP,
+socket.IP_ADD_MEMBERSHIP,
+socket.inet_aton(MCAST_ADDR) + socket.inet_aton(ANY))
+
+# setblocking(0) is equiv to settimeout(0.0) which means we poll the socket.
+# But this will raise an error if recv() or send() can't immediately find or send data. 
+sock.setblocking(0.1)
+
+print "begin loop!"
+
+while 1:
+    try:
+        data, addr = sock.recvfrom(1024)
+    except socket.error as e:
+        pass
+    else:
+        print "From: ", addr
+        print "Data: ", data
+print "end"
---
+++ send_multicast.py
@@ -0,0 +1,29 @@
+import socket
+
+# 224.0.0.1 is the all-hosts group. If you ping that group, all multicast capable hosts on the network should answer, 
+# as every multicast capable host must join that group at start-up on all it's multicast capable interfaces.
+MCAST_GRP = '224.0.0.1'
+#MCAST_GRP = '239.255.255.250'
+MCAST_PORT = 3702
+HOST_IP = "192.168.0.121"
+
+# TTL (time to live)
+# http://www.tldp.org/HOWTO/Multicast-HOWTO-2.html
+#    0 Restricted to the same host. Won't be output by any interface.
+#    1 Restricted to the same subnet. Won't be forwarded by a router.
+#  <32 Restricted to the same site, organization or department.
+#  <64 Restricted to the same region.
+# <128 Restricted to the same continent.
+# <255 Unrestricted in scope. Global.
+
+# THIS SENDS FROM DEFAULT INTERFACE (10.50.x.x)
+#sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+#sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 33)
+
+# SEND FROM HOST_IP ON SUBNET
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+sock.setsockopt(socket.SOL_IP, socket.IP_MULTICAST_IF, socket.inet_aton(HOST_IP))
+
+packetdata = open('discovery-probe.xml', "r").read()
+sock.sendto(packetdata, (MCAST_GRP, MCAST_PORT))
+print "SENT PACKET:\n\n" + packetdata
